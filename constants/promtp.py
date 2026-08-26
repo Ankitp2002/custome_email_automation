6f2ed2d3ce5d1d34ffd3fbd3374d621bfd149b6b
_Ankitdeveloper <ankit.prajapati@scriptalldna.com>
2026-08-26 23:11:55 +0530

    add smpt service, llm agents, prompt for cold email,
    panding point> fastapi orchestration with health and remainign apis, scraping pipeline, idea for feature extend and build.

---
+++ constants/promtp.py
@@ -0,0 +1,4 @@
+cold_email_prompt = """
+            Write a short, personalized cold outreach email to {email}. 
+            Their business description: {meta_info}. Keep it under 80 words.
+        """
